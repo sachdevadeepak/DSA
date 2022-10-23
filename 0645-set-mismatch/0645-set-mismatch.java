83190a980077457c2919bf1260eef27a9938ea1b
Deepak Sachdeva <66255751+sachdevadeepak@users.noreply.github.com>
2022-10-23 08:50:40 +0530

    Time: 49 ms (7.03%), Space: 60.7 MB (5.11%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,17 +1,17 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
-        int dup = -1, missing = -1;
+        Map < Integer, Integer > map = new HashMap();
+        int dup = -1, missing = 1;
+        for (int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
         for (int i = 1; i <= nums.length; i++) {
-            int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i)
-                    count++;
-            }
-            if (count == 2)
-                dup = i;
-            else if (count == 0)
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2)
+                    dup = i;
+            } else
                 missing = i;
         }
-        return new int[] {dup, missing};
+        return new int[]{dup, missing};
     }
 }
