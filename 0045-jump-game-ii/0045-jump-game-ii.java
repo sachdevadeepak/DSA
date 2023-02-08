17e356243c008195288b9035d84264c88fc496a7
Deepak Sachdeva <66255751+sachdevadeepak@users.noreply.github.com>
2023-02-08 07:36:52 +0530

    Time: 384 ms (5.41%), Space: 42 MB (99.95%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int jump(int[] nums) 
+    {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j] >= i-j)
+                {
+                    dp[i] = Math.min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
