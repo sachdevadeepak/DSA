a321203d3b7f1922c708b72084c217fa71b6ef54
Deepak Sachdeva <66255751+sachdevadeepak@users.noreply.github.com>
2023-05-29 02:11:07 +0530

    Time: 2 ms (88.89%), Space: 44 MB (11.11%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -1,5 +1,10 @@
 class Solution {
-   public String removeTrailingZeros(String num) {
-        return num.replaceAll("0+$", "");
+  public String removeTrailingZeros(String num) {
+    int len = num.length();
+    while (--len >= 0) {
+        if (num.charAt(len) != '0') 
+            break;
     }
+    return num.substring(0, ++len);
+}
 }
